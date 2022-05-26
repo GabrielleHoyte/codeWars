abb72fa11240ef5640b4d4077ce120f16cca5dd9
GabrielleHoyte <gabriellehoyte@gmail.com>
2022-05-26 14:09:43 -0400

    codewars commit

---
+++ trainingJSNine/main.js
@@ -0,0 +1,12 @@
+function padIt(str,n){
+  //coding here
+  let i = 0
+  while (i<n){
+    if(i%2===0){
+      str = '*' + str
+    } else {
+      str = str + '*'
+    }
+    i++
+  }return str
+} 
